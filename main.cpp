6c8b89e4d0a93d54eed1d1e37a6b91fb05ca3557
Angrybeef1 <136150862+Angrybeef1@users.noreply.github.com>
2024-11-15 08:50:30 -0800

    3rd commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //COMSC 210 | LAb 32 | Robert Stonemetz
 
-#include "Car.h"
+#include "Car-1.h"
 #include <deque>
 #include <random>
 #include <ctime>
@@ -8,11 +8,33 @@
 const int QUEUE_SIZE = 2;
 const double PAY_TOLL = 0.55;
 
-Void Print Queue(){
+void Print Queue(const deque<car>& queue){
+    cout << "Queue:\n";
+    if (queue.empty()){
+        cout << "\n";
+        return;
+    }
+    for (const Car& car : queue){
+        cout << "    ";
+        car.print();
+    }
 
 }
 
 
 int main (){
+    srand(time(0));
+
+    cout << "Initial queue:\n";
+    printQueue();
+    cout << endl;
+
+    int time = 1;
     
+    while(){
+
+    }
+
+    return 0;
+
 }
