a407287b732b52bcce2e8fac2ee067e63213f51b
Angrybeef1 <136150862+Angrybeef1@users.noreply.github.com>
2024-11-15 08:40:52 -0800

    second commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//COMSC 210 | LAb 32 | Robert Stonemetz
+
+#include "Car.h"
+#include <deque>
+#include <random>
+#include <ctime>
+
+const int QUEUE_SIZE = 2;
+const double PAY_TOLL = 0.55;
+
+Void Print Queue(){
+
+}
+
+
+int main (){
+    
+}
