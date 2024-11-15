e4ae8ade7264cb95c64cfb72c95afcee76f86873
Angrybeef1 <136150862+Angrybeef1@users.noreply.github.com>
2024-11-15 08:57:43 -0800

    3rd commit

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 const int QUEUE_SIZE = 2;
 const double PAY_TOLL = 0.55;
 
-void Print Queue(const deque<car>& queue){
+void PrintQueue(const deque<Car>& queue){
     cout << "Queue:\n";
     if (queue.empty()){
         cout << "\n";
@@ -25,14 +25,34 @@ void Print Queue(const deque<car>& queue){
 int main (){
     srand(time(0));
 
+    deque<Car> tollQueue;
+    for(int i = 0; i< QUEUE_SIZE; i++){
+        tollQueue.push_back(Car());
+    }
+
+
     cout << "Initial queue:\n";
-    printQueue();
+    printQueue(tollQueue);
     cout << endl;
 
     int time = 1;
     
-    while(){
+    while(!tollQueue.empty()){
+        double probability = (double)rand() /RAND_MAX;
 
+        if (probability < PAY_TOLL && !tollQueue.empty()){
+            cout << "Time: " << time << " Operation: Car paid: ";
+            tollQueue.front().print();
+            tollQueue.pop_front();
+        }else {
+            Car newCar;
+            cout << "Time: " << time << " Operation: Joined lane: ";
+            newCar.print();
+            tollQueue.push_back(newCar);
+        }
+        printQueue (tollQueue);
+        cout << endl;
+        time++;
     }
 
     return 0;
